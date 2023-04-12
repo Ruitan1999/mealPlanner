928015d421090e61ed95ce4bf29cb19259fa7a7c
Ruitan Huang <ruitan1520@gmail.com>
2023-04-12 13:28:27 +0800

    Back to the top button

---
+++ Components/BackToTopButton.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { FaArrowCircleUp } from "react-icons/fa";
+import styles from "./BackToTopButton.module.css";
+
+const BackToTopButton: React.FC = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  const handleScroll = () => {
+    setShowButton(window.scrollY > 100);
+  };
+
+  const handleButtonClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <button
+      className={`${styles.backToTopButton} ${showButton ? styles.show : ""}`}
+      onClick={handleButtonClick}
+      aria-label="Back to Top"
+    >
+      <FaArrowCircleUp />
+    </button>
+  );
+};
+
+export default BackToTopButton;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import BackToTopButton from "../Components/BackToTopButton";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
         <main>
           <Component {...pageProps} />
+          <BackToTopButton />
         </main>
         <Footer />
 
